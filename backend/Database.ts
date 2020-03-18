a9633a82a7c21e7bbcdf85b992b6a31460d2edc0
Masha <mashushvik.2001@gmail.com>
2020-03-18 21:35:49 +0200

    feat(db): add generalized queries

--- backend/Database.ts
+++ backend/Database.ts
@@ -1,17 +1,34 @@
 import * as SQLite from 'expo-sqlite';
 import { Piece } from "../types/Piece";
+import { dbHelpers } from './dbCommon';
+import { createOneToManyTable } from './dbCommon';
+
+const AuthorItem = 'author';
+const TagItem = 'tag';
+
+const rowToPiece = (row: any) => ({
+    name: row.pieceName,
+    timeSpent: 0,
+    notificationsOn: false,
+    notificationsInterval: 2,
+    authors: row.authors.split(','),
+    lastPracticedOn: new Date(row.lastPracticedOnDate),
+});
 
 export default () => {
     const db = SQLite.openDatabase('PracticeManager');
+    const { insertOneToManyItem } = dbHelpers(db);
 
     db.transaction(tx => {
         tx.executeSql(
             'CREATE TABLE IF NOT EXISTS Pieces (' +
             'id integer primary key not null,' +
             ' pieceName varchar(225) not null,' +
-            ' authors varchar(255),' +
             ' addedOnDate timestamp not null,' +
-            ' lastPracticedOnDate timestamp)',
+            ' lastPracticedOnDate timestamp,' +
+            'notificationsOn smallint default 0,' +
+            'notifications interval smallint default 3' +
+            ')',
             [],
             undefined,
             (_tr, err) => {
@@ -19,36 +36,12 @@ export default () => {
                 return false
             });
 
-        tx.executeSql('CREATE TABLE IF NOT EXISTS Tag (id integer primary key not null, name varchar(225) not null)',
-            [],
-            undefined,
-            (_tr, err) => {
-                console.log('err creating table tags', err);
-                return false
-            }
-        );
-
-        tx.executeSql('CREATE TABLE IF NOT EXISTS PieceTag (tagId integer not null, pieceId integer not null )',
-            [],
-            undefined,
-            (_tr, err) => {
-                console.log('err creating table piece tags', err);
-                return false
-            }
-        );
+        createOneToManyTable(AuthorItem, tx);
+        createOneToManyTable(TagItem, tx);
     });
 
-    const rowToPiece = (row: any) => ({
-        name: row.pieceName,
-        timeSpent: 0,
-        notificationsOn: false,
-        notificationsInterval: 2,
-        authors: row.authors.split(','),
-        lastPracticedOn: new Date(row.lastPracticedOnDate),
-    });
-
-    const getAllPieces = async (): Promise<Piece[]> =>
-        new Promise((resolve, reject) =>
+    const getAllPieces = async (): Promise<Piece[]> => {
+        return new Promise((resolve, reject) =>
             db.transaction(tx =>
                 tx.executeSql('SELECT * FROM Pieces',
                     [],
@@ -61,20 +54,47 @@ export default () => {
                         return false
                     }
                 )));
+    };
 
-    const addPiece = async (piece: Piece): Promise<void> =>
-        new Promise((_resolve, reject) =>
+    const addPiece = async (piece: Piece): Promise<void> => {
+        const pieceId = await insertPiece(piece);
+
+        if (piece.authors !== undefined) {
+            await Promise.all(piece.authors.map(author => insertPieceAuthor(pieceId, author)));
+        }
+
+        if (piece.tags !== undefined) {
+            await Promise.all(piece.tags.map(tag => insertPieceTag(pieceId, tag)));
+        }
+    };
+
+    const insertPiece = async (piece: Piece): Promise<number> => {
+        return new Promise((resolve, reject) =>
             db.transaction(tx => {
-                tx.executeSql(`INSERT INTO Pieces (pieceName, authors, addedOnDate)
-                               values (?, ?, ?)`,
-                    [piece.name, piece.authors, Date.now()],
-                    undefined,
+                tx.executeSql(`INSERT INTO Pieces (pieceName, addedOnDate, notificationsOn, notificationsInterval)
+                               values (?, ?, ?, ?)`,
+                    [
+                        piece.name,
+                        Date.now(),
+                        piece.notificationsOn,
+                        piece.notificationsInterval
+                    ],
+                    (_tr, { insertId }) => resolve(insertId),
                     (_tr, err) => {
                         reject(err);
                         return false;
                     }
                 )
             }));
+    };
+
+    const insertPieceAuthor = async (pieceId: number, author: string): Promise<void> => {
+        await insertOneToManyItem(pieceId, author, AuthorItem);
+    };
+
+    const insertPieceTag = async (pieceId: number, tag: string): Promise<void> => {
+        await insertOneToManyItem(pieceId, tag, TagItem,);
+    };
 
     const getPiecesByName = async (name: string): Promise<Piece[]> =>
         new Promise((resolve, reject) => db.transaction(tx => {
