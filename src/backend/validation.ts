8e39d770c1d9de8bd0b293db118c8bcaee2f8976
Masha <mashushvik.2001@gmail.com>
2020-03-23 17:58:11 +0200

    refactor(pieceValidation): fix author comparison & update error messages

--- src/backend/validation.ts
+++ src/backend/validation.ts
@@ -1,4 +1,5 @@
 import { getRepository } from "typeorm";
+import { Piece } from "../types/Piece";
 import { PieceEntity } from "./entity/Piece";
 
 export type CheckResult = {
@@ -6,19 +7,33 @@ export type CheckResult = {
     errors?: string,
 }
 
-export const validatePiece = async (piece: PieceEntity): Promise<CheckResult> => {
+export const validatePiece = async (piece: Piece): Promise<CheckResult> => {
     if (piece.name.length === 0) {
-        return Promise.resolve({ valid: false, errors: 'Piece title should be filled' });
+        return Promise.resolve({ valid: false, errors: 'You forgot to enter piece title ✍' });
     }
 
     const pieceRepo = getRepository(PieceEntity);
     const piecesWithSameName = await pieceRepo.find({ where: { name: piece.name }, relations: ['authors'] });
 
     if (piecesWithSameName.length > 0) {
-        if (piecesWithSameName.find(item => item.authors === piece.authors) !== undefined) {
-            return Promise.resolve({ valid: false, errors: 'Piece with same name and author(s) already exists' });
+        if (piecesWithSameName.find(item => areAuthorsSame(item, piece) !== undefined)) {
+            return Promise.resolve({
+                valid: false,
+                errors: 'A piece with the same name and author(s) already exists 🤷‍'
+            });
         }
     }
 
     return Promise.resolve({ valid: true });
+};
+
+const areAuthorsSame = (piece1: PieceEntity, piece2: Piece) => {
+    if (piece1.authors.length === 0 && piece2.authors.length === 0) {
+        return true;
+    } else if (piece2.authors.find(
+        (author) => piece1.authors.find(a => a.name !== author) !== undefined)) {
+        return false;
+    }
+
+    return true;
 };
