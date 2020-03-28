46fc21584de42c1944b7af52451adeef27d0fdfa
Masha <mashushvik.2001@gmail.com>
2020-03-28 18:21:57 +0200

    feat(appNavigation): wrap stack navigator with drawer

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,7 +1,9 @@
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator, HeaderBackButton } from "@react-navigation/stack";
 import React from 'react';
-import { AppHeaderStyle, AppSidePadding } from "./AppStyle";
+import { AppHeaderStyle, AppSidePadding, DrawerContentStyle, DrawerStyle } from "./AppStyle";
+import { CustomDrawerContent } from "./components/basic/CustomDrawer";
 import { NavIcon } from "./components/basic/icons/Header";
 import { Dashboard } from "./components/Dashboard";
 import { PieceForm } from "./components/Pieces/PieceForm";
@@ -41,53 +43,62 @@ const options = {
 };
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export const Main = () => (
     <NavigationContainer>
-        <Stack.Navigator
-            screenOptions={{
-                ...options,
-                cardOverlayEnabled: true,
-                gestureEnabled: true,
-                ...MyTransition,
-            }}
-
-            initialRouteName={INITIAL_SCREEN}>
-
-            <Stack.Screen
-                options={{
-                    headerLeft: () => <NavIcon onPress={() => console.log('gdgf')}/>,
-                }}
-                name={REPERTOIRE}
-                component={RepertoireScreen}/>
-
-            <Stack.Screen name={PIECE} component={PieceScreen}
-                          options={({ navigation }) => ({
-                              title: '',
-                              headerLeft: () => <HeaderBackButton onPress={() => navigation.navigate(REPERTOIRE)}/>,
-                              headerTransparent: true,
-                          })}
-            />
-            <Stack.Screen name={PIECE_FORM} component={PieceForm}
-                          options={({ route }) => ({
-                              headerTransparent: true,
-                              title: route.params?.mode === ActionType.Edit ? 'Edit piece' : 'Add piece',
-                          })}/>
-
-            <Stack.Screen name={SESSION_PLAN_LIST}
-                          component={SessionPlansList}/>
-            <Stack.Screen name={SESSION_PLAN} component={SessionPlanScreen}/>
-            <Stack.Screen name={SESSION_PLAN_FORM} component={SessionPlanForm}/>
-
-            <Stack.Screen name={SESSION_START} component={SessionStartScreen}/>
-
-            <Stack.Screen name={PLANNED_SESSION_TIMER} component={PlannedSessionTimer}/>
-            <Stack.Screen name={FREE_SESSION_TIMER} component={FreeSessionTimer}/>
-            <Stack.Screen name={FREE_SESSION_ACTIVITY_CHOICE} component={FreeSessionActivityChoice}/>
-            <Stack.Screen name={SESSION_END} component={SessionEndScreen}/>
-
-            <Stack.Screen name={DASHBOARD} component={Dashboard}/>
-            <Stack.Screen name={SETTINGS} component={AppSettings}/>
-        </Stack.Navigator>
+        <Drawer.Navigator
+            drawerStyle={DrawerStyle}
+            drawerContentOptions={{ contentContainerStyle: DrawerContentStyle }}
+            drawerContent={props => (<CustomDrawerContent {...props} />)}>
+            <Drawer.Screen name='Root' component={Root}/>
+        </Drawer.Navigator>
     </NavigationContainer>
+);
+
+const Root = () => (
+    <Stack.Navigator
+        screenOptions={{
+            ...options,
+            cardOverlayEnabled: true,
+            gestureEnabled: true,
+            ...MyTransition,
+        }}
+        initialRouteName={INITIAL_SCREEN}>
+
+        <Stack.Screen
+            options={({ navigation }) => ({
+                headerLeft: () => <NavIcon onPress={() => navigation.toggleDrawer()}/>,
+            })}
+            name={REPERTOIRE}
+            component={RepertoireScreen}/>
+
+        <Stack.Screen name={PIECE} component={PieceScreen}
+                      options={({ navigation }) => ({
+                          title: '',
+                          headerLeft: () => <HeaderBackButton onPress={() => navigation.navigate(REPERTOIRE)}/>,
+                          headerTransparent: true,
+                      })}
+        />
+        <Stack.Screen name={PIECE_FORM} component={PieceForm}
+                      options={({ route }) => ({
+                          headerTransparent: true,
+                          title: route.params?.mode === ActionType.Edit ? 'Edit piece' : 'Add piece',
+                      })}/>
+
+        <Stack.Screen name={SESSION_PLAN_LIST}
+                      component={SessionPlansList}/>
+        <Stack.Screen name={SESSION_PLAN} component={SessionPlanScreen}/>
+        <Stack.Screen name={SESSION_PLAN_FORM} component={SessionPlanForm}/>
+
+        <Stack.Screen name={SESSION_START} component={SessionStartScreen}/>
+
+        <Stack.Screen name={PLANNED_SESSION_TIMER} component={PlannedSessionTimer}/>
+        <Stack.Screen name={FREE_SESSION_TIMER} component={FreeSessionTimer}/>
+        <Stack.Screen name={FREE_SESSION_ACTIVITY_CHOICE} component={FreeSessionActivityChoice}/>
+        <Stack.Screen name={SESSION_END} component={SessionEndScreen}/>
+
+        <Stack.Screen name={DASHBOARD} component={Dashboard}/>
+        <Stack.Screen name={SETTINGS} component={AppSettings}/>
+    </Stack.Navigator>
 );
