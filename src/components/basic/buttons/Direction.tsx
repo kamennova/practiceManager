e7d1e9d5591095d25e5d22ae3b8e66676a32a3bd
Masha <mashushvik.2001@gmail.com>
2020-05-18 17:37:29 +0300

    fix(directionBtns): fix direction btns color in dark theme

--- src/components/basic/buttons/Direction.tsx
+++ src/components/basic/buttons/Direction.tsx
@@ -1,32 +1,22 @@
 import React from 'react';
-import { Image, TouchableHighlight, View } from "react-native";
+import { TouchableHighlight, View } from "react-native";
 import { DirectionButtonStyle } from "../../../AppStyle";
 import { useTheme } from "../../../theme";
+import { Direction } from "../../../types/Direction";
+import { ChevronIcon } from "../icons/Chevron";
 
 type NavBtnProps = {
     onPress?: () => void
 };
 
-export const PrevButton = (props: NavBtnProps) => {
-    const style = {
-        ...DirectionButtonStyle(useTheme().colors, props.onPress === undefined),
-        marginRight: 'auto',
-    };
-
-    return (
-        <TouchableHighlight onPress={props.onPress}>
-            <View style={style}>
-                <Image style={{ width: 20, height: 20 }} source={require('../../../../assets/left.png')}/>
-            </View>
-        </TouchableHighlight>
-    );
-};
-
-export const NextButton = (props: NavBtnProps) => (
+const DirectionBtn = (props: { onPress?: () => void, direction: Direction }) => (
     <TouchableHighlight onPress={props.onPress}>
         <View style={DirectionButtonStyle(useTheme().colors, props.onPress === undefined)}>
-            <Image style={{ transform: [{ rotate: '180deg' }], width: 20, height: 20 }}
-                   source={require('../../../../assets/left.png')}/>
+            <ChevronIcon direction={props.direction}/>
         </View>
     </TouchableHighlight>
 );
+
+export const PrevButton = (props: NavBtnProps) => <DirectionBtn direction={Direction.Left} onPress={props.onPress}/>;
+
+export const NextButton = (props: NavBtnProps) => <DirectionBtn direction={Direction.Right} onPress={props.onPress}/>;
